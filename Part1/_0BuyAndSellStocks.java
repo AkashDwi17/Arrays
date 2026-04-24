25662f31b4f9d658a6c27d2f5d4cb38a84b8c47f
Akash Dwivedi <akashdwi17@gmail.com>
2026-04-24 08:20:32 +0530

    Buy And Sell Stocks

---
+++ Part1/_0BuyAndSellStocks.java
@@ -0,0 +1,21 @@
+package Part1;
+
+public class _0BuyAndSellStocks {
+    public static int buyAndSell (int price[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i=0; i<price.length; i++){
+            if (buyPrice < price[i]){
+                int profit = price[i] - buyPrice;
+                maxProfit = Math.max(profit, maxProfit);
+            }else{
+                buyPrice = price[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int price[] = {7,1,5,3,6,4};
+        System.out.print (buyAndSell(price));
+    }
+}
